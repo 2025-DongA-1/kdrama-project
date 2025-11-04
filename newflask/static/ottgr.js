fc2e5f107cffd645278f68dd95097c0f4e725aed
Eunseo <silverwest0539@gmail.com>
2025-11-05 08:13:52 +0900

    장르별 통합 분석 js, json 파일 업로드

---
+++ newflask/static/ottgr.js
@@ -0,0 +1,39 @@
+fetch('static/ott_genre_counts.json')
+  .then(response => response.json())
+  .then(data => {
+    // 장르 이름 (x축 라벨)
+    const labels = data.map(item => item.MAIN_GENRE);
+
+    const ctx = document.getElementById('ottChart');
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: '2010년대',
+            data: data.map(item => item["2010년대"]), // 2010년대 데이터
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1
+          },
+          {
+            label: '2020년대',
+            data: data.map(item => item["2020년대"]), // 2020년대 데이터
+            backgroundColor: 'rgba(54, 162, 235, 0.5)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  })
+  .catch(err => console.error("JSON 불러오기 실패:", err));

9c75e0ac7c8958fa1b0069aa2fb5fe30398e38c2
Eunseo <silverwest0539@gmail.com>
2025-11-04 17:17:56 +0900

    평점 분석 결과 폴더 추가

---
+++ Rating/JS/brodchart2020.js
@@ -0,0 +1,40 @@
+fetch('../DATA/brod2020_chart_data.json')
+    .then(response => response.json())
+    .then(data => {
+        const ctx = document.getElementById('myBarChart2020').getContext('2d');
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: data.labels,
+                datasets: [{
+                    label: '값',
+                    data: data.values,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: { y: { 
+                    beginAtZero: true,
+                    min: 8.2,
+                    maz: 9
+                 } }
+            }
+        });
+    })
+    .catch(err => console.error("JSON 불러오기 실패:", err));
